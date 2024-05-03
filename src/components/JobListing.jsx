d006dd74de7aafb1d2b2e0095c64e88d77f20925
selvam44 <selvam.v.chandran@gmail.com>
2024-05-03 08:35:22 +0530

    feat : some more features added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-
+import { useState } from 'react';
+import {FaMapMarker} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 const JobListing = ({job}) => {
+  
+  const[showFullDescription,setShowFullDescription] =  useState(false);  
+  let description = job.description;
+
+  if(!showFullDescription){
+    description = description.substring(0,90) +'...';
+  }
+
   return (
    
    <div className="bg-white rounded-xl shadow-md relative">
@@ -11,24 +21,27 @@ const JobListing = ({job}) => {
               </div>
 
               <div className="mb-5">
-                {job.description}
+                {description}
               </div>
-
+              <button onClick={()=>setShowFullDescription((prevState)=>!prevState)} 
+              className="text-indigo-500 mb-5 hover:text-indigo-600 "
+              >{showFullDescription ? 'Less' : 'More'} </button>
               <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
               <div className="border border-gray-100 mb-5"></div>
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
+                  {/* <i className="fa-solid fa-location-dot text-lg"></i> */}
+                  <FaMapMarker className='inline text-large mb-1 mr-1'/>
                   {job.location}
                 </div>
-                <a
-                  href={`/job/${job.id}`}
+                <Link
+                  to={`/job/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
           </div>
